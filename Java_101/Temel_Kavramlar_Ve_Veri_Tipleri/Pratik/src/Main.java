9a7583702346f517c85f86f0a3fbf8830d86901a
umut guven <ggvenumut@gmail.com>
2023-03-09 17:26:02 +0300

    JAVA-101 / Temel Kavramlar ve Veri Tipleri  -Not Ortalamasi Hesaplayan Program

---
+++ Java_101/Temel_Kavramlar_Ve_Veri_Tipleri/Pratik/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Degiskenler
+        int mat, fizik,kimya,turkce,tarih,muzik;
+
+        // Scanner Sinifini Dahil Etme
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanicidan Degerleri Alma
+        System.out.println("Matematik Notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.println("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.println("Turkce Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.println("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.println("Muzik Notunuz : ");
+        muzik = inp.nextInt();
+
+
+        int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam / 6.0;
+        System.out.println(sonuc);
+
+    }
+}
