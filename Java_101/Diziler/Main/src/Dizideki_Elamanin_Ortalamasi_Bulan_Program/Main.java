f2edb56447d50ab23e28389a2d52d426b5904b40
umut guven <ggvenumut@gmail.com>
2023-03-10 17:52:47 +0300

    JAVA-101 / Diziler  - Dizideki Elamanlarin Ortalamasini Bulan Program

---
+++ Java_101/Diziler/Main/src/Dizideki_Elamanin_Ortalamasi_Bulan_Program/Main.java
@@ -0,0 +1,16 @@
+package Dizideki_Elamanin_Ortalamasi_Bulan_Program;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] list = {1,2,3,4,5};
+        double sum = 0.0;
+
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+
+        double avarage = sum / list.length;
+        System.out.println(avarage);
+
+    }
+}
---
+++ Java_101/Diziler/Main/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] list = new int[10];
+
+
+    }
+}
